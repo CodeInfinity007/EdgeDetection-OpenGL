02d11d6e4b0e858f43644c7915191f540a1f26a4
CodeInfinity007 <divyamjps@gmail.com>
2025-10-09 02:04:40 +0530

    JNI
    integrated OpenCV processing via JNI (grayscale + Canny)

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,13 +1,26 @@
 #include <jni.h>
 #include <opencv2/opencv.hpp>
 
-extern "C" JNIEXPORT void JNICALL
+extern "C"
+JNIEXPORT void JNICALL
 Java_com_div_edgedetectionapp_NativeLib_processFrame(
     JNIEnv* env,
     jobject thiz,
     jbyteArray input,
     jint width,
     jint height,
-    jbyteArray output) {
-    // TODO
+    jbyteArray output
+) {
+    jbyte* inBytes = env->GetByteArrayElements(input, NULL);
+    jbyte* outBytes = env->GetByteArrayElements(output, NULL);
+
+    cv::Mat gray(height, width, CV_8UC1, reinterpret_cast<unsigned char*>(inBytes));
+
+    cv::Mat edges;
+    cv::Canny(gray, edges, 50, 150);
+
+    memcpy(outBytes, edges.data, width * height);
+
+    env->ReleaseByteArrayElements(input, inBytes, 0);
+    env->ReleaseByteArrayElements(output, outBytes, 0);
 }
--- app/src/main/java/com/div/edgedetectionapp/MainActivity.kt
+++ app/src/main/java/com/div/edgedetectionapp/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.div.edgedetectionapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.app.Activity
+import android.media.ImageReader
+import android.media.Image
+import android.graphics.ImageFormat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+    private lateinit var imageReader: ImageReader
+    private val width = 640
+    private val height = 480
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageReader = ImageReader.newInstance(width, height, ImageFormat.YUV_420_888, 2)
+        imageReader.setOnImageAvailableListener({ reader ->
+            val image: Image = reader.acquireLatestImage() ?: return@setOnImageAvailableListener
+
+            val yPlane = image.planes[0]
+            val buffer = yPlane.buffer
+            val rowStride = yPlane.rowStride
+            val padded = ByteArray(rowStride * height)
+            buffer.get(padded)
+
+            val inputArray = ByteArray(width * height)
+            for (r in 0 until height) {
+                System.arraycopy(padded, r * rowStride, inputArray, r * width, width)
+            }
+
+            val outputArray = ByteArray(width * height)
+            NativeLib.processFrame(inputArray, width, height, outputArray)
+
+            image.close()
+        }, null)
     }
 }
